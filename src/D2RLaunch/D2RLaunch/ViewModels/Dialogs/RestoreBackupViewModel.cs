dc4721be9a555bdc83e507c8dc8450746b3dfa46
locbones <41172978+locbones@users.noreply.github.com>
2024-02-26 10:09:21 -0700

    Restore Character Improvement
    - User will be directed to selected character's backup folder upon selection to select the timestamped file to restore
    - OnRestoreBackupSelection() created and OnRestoreBackup() modified

--- src/D2RLaunch/D2RLaunch/ViewModels/Dialogs/RestoreBackupViewModel.cs
+++ src/D2RLaunch/D2RLaunch/ViewModels/Dialogs/RestoreBackupViewModel.cs
@@ -4,16 +4,19 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using Caliburn.Micro;
 using JetBrains.Annotations;
 using ILog = log4net.ILog;
 using LogManager = log4net.LogManager;
+using WinForms = System.Windows.Forms;
 
 namespace D2RLaunch.ViewModels.Dialogs
 {
-    public class RestoreBackupViewModel : Screen
+    public class RestoreBackupViewModel : Caliburn.Micro.Screen
     {
         #region members
 
@@ -21,6 +24,7 @@ namespace D2RLaunch.ViewModels.Dialogs
         private ILog _logger = LogManager.GetLogger(typeof(RestoreBackupViewModel));
         private string _selectedCharacter;
 
+        OpenFileDialog openFileDialog = new OpenFileDialog();
         #endregion
 
         public RestoreBackupViewModel(ShellViewModel shellViewModel)
@@ -78,18 +82,27 @@ namespace D2RLaunch.ViewModels.Dialogs
         }
 
         [UsedImplicitly]
+        public async void OnRestoreBackupSelection()
+        {
+            openFileDialog.ShowDialog();
+            openFileDialog.InitialDirectory = Path.Combine(ShellViewModel.BackupFolder, SelectedCharacter);
+            openFileDialog.Filter = "D2 Save Files (*.d2s)|*.d2s";
+        }
+
+            [UsedImplicitly]
         public async void OnRestoreBackup()
         {
             string characterPath = Path.Combine(ShellViewModel.BackupFolder, SelectedCharacter);
             string stashPath = Path.Combine(ShellViewModel.BackupFolder, "Stash");
 
             if (Directory.Exists(characterPath))
-                File.Copy(Path.Combine(characterPath, $"{SelectedCharacter}.d2s"), Path.Combine(ShellViewModel.SaveFilesFilePath, $"{SelectedCharacter}.d2s"), true);
+                File.Copy(openFileDialog.FileName, Path.Combine(ShellViewModel.SaveFilesFilePath, $"{SelectedCharacter}.d2s"), true);
 
             if (Directory.Exists(stashPath))
                 File.Copy(Path.Combine(stashPath, "SharedStashSoftCoreV2.d2i"), Path.Combine(ShellViewModel.SaveFilesFilePath, "SharedStashSoftCoreV2.d2i"), true);
 
-            MessageBox.Show($"{SelectedCharacter} Restored!", "Restore Complete", MessageBoxButton.OK, MessageBoxImage.Information);
+            System.Windows.MessageBox.Show($"{SelectedCharacter} Restored!", "Restore Complete", MessageBoxButton.OK, MessageBoxImage.Information);
+            await TryCloseAsync();
         }
     }
 }
